86477e24cb20f22e1909ed68e6e92c4676458eb3
Ayrton W <youngwilliam329495@gmail.com>
2026-04-08 02:51:04 -0300

    Add PWA support with manifest and service worker

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const cacheName = 'simulador-renda-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))
+    ))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
